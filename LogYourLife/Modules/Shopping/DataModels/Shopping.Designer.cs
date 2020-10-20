40aefabaab95fd9a45d6eedeb918fea85c707638
Andorim <dev@andorim.de>
2020-10-20 07:10:09 +0200

    added discriptions added Module folders

--- LogYourLife/Modules/Shopping/DataModels/Shopping.Designer.cs
+++ LogYourLife/Modules/Shopping/DataModels/Shopping.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Die T4-Codegenerierung ist für Modell 'D:\onedrive\Documents\Projekte\VisualStudio\LogYourLife\LogYourLife\DataModels\Shopping.edmx' aktiviert. 
+﻿// Die T4-Codegenerierung ist für Modell 'C:\Users\sgnir\Source\Repos\andorim\LogYourLife\LogYourLife\Modules\Shopping\DataModels\Shopping.edmx' aktiviert. 
 // Um die Generierung von Legacycode zu aktivieren, ändern Sie den Wert der Designer-Eigenschaft 'Code Generation Strategy'
 // in 'Legacy ObjectContext'. Diese Eigenschaft wird im Eigenschaftenfenster angezeigt, wenn das Modell
 // im Designer geöffnet ist.
